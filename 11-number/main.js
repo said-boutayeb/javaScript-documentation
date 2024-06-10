95cad2038788afaca955dc20943a17287d7813cf
said boutayeb <saidsimo115@gmail.com>
2024-06-10 03:04:39 +0200

    V.01

---
+++ 11-number/main.js
@@ -0,0 +1,30 @@
+/*
+------------Number
+-----::> Double Precision
+-----::> Syntactic
+-----::> e
+-----::> **
+-----::> With Decimal
+-----::> Number + BigInt
+-----::> Number + Min Value
+-----::> Number Max Value
+*/
+
+console.log("console.log(1000000);");
+console.log(1000000);
+console.log("console.log(1_000_000);");
+console.log(1_000_000);
+console.log("console.log(1e6)");
+console.log(1e6);
+console.log("console.log(10 ** 6)");
+console.log(10 ** 6);
+console.log("console.log(10 * 10 * 10 * 10 * 10 * 10 )");
+console.log(10 * 10 * 10 * 10 * 10 * 10 );
+console.log("console.log(1000000.0)");
+console.log(1000000.0);
+
+console.log(Number.MAX_SAFE_INTEGER);
+console.log(Number.MAX_VALUE);
+console.log(Number.MAX_VALUE + 2);
+console.log(Number.MIN_VALUE);
+console.log(Number.MIN_VALUE - 2);// I'm not understand <<<<<<<<<<<<<<<
