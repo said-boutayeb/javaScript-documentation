b62fc2c4290e27b52cad1cb32344d0ba6a702be1
said boutayeb <saidsimo115@gmail.com>
2024-06-11 04:36:48 +0200

    V.02

--- 11-number/main.js
+++ 11-number/main.js
@@ -6,7 +6,7 @@
 -----::> **
 -----::> With Decimal
 -----::> Number + BigInt
------::> Number + Min Value
+-----::> Number Min Value
 -----::> Number Max Value
 */
 
@@ -19,12 +19,13 @@ console.log(1e6);
 console.log("console.log(10 ** 6)");
 console.log(10 ** 6);
 console.log("console.log(10 * 10 * 10 * 10 * 10 * 10 )");
-console.log(10 * 10 * 10 * 10 * 10 * 10 );
+console.log(10 * 10 * 10 * 10 * 10 * 10);
 console.log("console.log(1000000.0)");
 console.log(1000000.0);
 
 console.log(Number.MAX_SAFE_INTEGER);
 console.log(Number.MAX_VALUE);
-console.log(Number.MAX_VALUE + 2);
+console.log(Number.MAX_VALUE + 28373);
 console.log(Number.MIN_VALUE);
-console.log(Number.MIN_VALUE - 2);// I'm not understand <<<<<<<<<<<<<<<
+console.log(Number.MIN_VALUE + 346342);// I'm not understand <<<<<<<<<<<<<<<
+
